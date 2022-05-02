e00de1f9217666c95c49326e44ccce698e103549
Kirishankerpro <kirishanker@gmail.com>
2022-05-02 15:40:27 +0200

    changes with axios

--- src/components/isLoading.js
+++ src/components/isLoading.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import Header from "./components/Header";
-import Main from "./components/Main";
 
 function App() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
-    const response = await axios.get(
-      "https://deliveroo-backend-kingx.herokuapp.com/infos"
-    );
+    const response = await axios.get("http://localhost:3100/");
     // console.log(response.data);
     setData(response.data);
     setIsLoading(false);
